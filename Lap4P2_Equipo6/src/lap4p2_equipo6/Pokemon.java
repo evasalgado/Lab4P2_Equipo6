150e5e1fc758b9aa2584494757e6d486fc0579b9
WalterjDm <walterjdoblado569@unitec.edu>
2023-08-11 16:51:39 -0600

    cambio de tipo

--- Lap4P2_Equipo6/src/lap4p2_equipo6/Pokemon.java
+++ Lap4P2_Equipo6/src/lap4p2_equipo6/Pokemon.java
@@ -8,7 +8,7 @@ public class Pokemon {
     private int nivel;
     private int xpAcum;
     private int cpNes;
-    private int []move = new int[4];
+    private Pokemon []move = new Pokemon[4];
     private int hp;
     private int atk;
     private int def;
@@ -68,11 +68,11 @@ public class Pokemon {
         this.cpNes = cpNes;
     }
 
-    public int[] getMove() {
+    public Pokemon[] getMove() {
         return move;
     }
 
-    public void setMove(int[] move) {
+    public void setMove(Pokemon[] move) {
         this.move = move;
     }
 
