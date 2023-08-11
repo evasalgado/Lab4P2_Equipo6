37d5077bf2bc2ee4a4b44918c021122c2e0a84f3
evasalgado <imviarsha@gmail.com>
2023-08-11 13:48:41 -0600

    Menu principal agregado

--- Lap4P2_Equipo6/src/lap4p2_equipo6/Lap4P2_Equipo6.java
+++ Lap4P2_Equipo6/src/lap4p2_equipo6/Lap4P2_Equipo6.java
@@ -1,20 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package lap4p2_equipo6;
 
-/**
- *
- * @author evaja
- */
+import java.util.Random;
+import java.util.Scanner;
+
+
 public class Lap4P2_Equipo6 {
 
-    /**
-     * @param args the command line arguments
-     */
+    static Scanner leer = new Scanner(System.in);
+    static Random alea = new Random();
     public static void main(String[] args) {
-        // TODO code application logic here
+        int op = 0;
+        while (op!=4) {            
+            System.out.println("Bienvenido al Pokedex\n"
+                + "1. Registrar Entrenador\n"
+                + "2. Battle Factory\n"
+                + "3. Capturar/ Entrenar\n"
+                + "4. Salir");
+            op=leer.nextInt();
+            switch (op) {
+                case 1:
+                    
+                    break;
+                case 2: 
+                    break;
+                case 3: 
+                    break;
+                case 4: 
+                    op=4;
+                    System.out.println("Gracias por usar el programa");
+                    break;
+                default:
+                    System.out.println("Numero ingresado no es valido");
+                    break;
+            }
+        }
     }
     
 }
